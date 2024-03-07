2280ce820b38f162219e620e971dd2e96b5e072e
Wisal Alaz <Wisalalaz10@gmail.com>
2024-03-07 09:30:51 +0000

    fonts and upload container

--- app/UploadCV/page.jsx
+++ app/UploadCV/page.jsx
@@ -4,32 +4,31 @@ import Nav from '../(components)/Nav'
 const UploadCV = () => {
   return (
     <>
-        <div>Please upload your resume down bellow this has to be a PDF file.</div>
-        <h1>Resume Upload </h1>
-        <form><p>Upload here</p></form>
-        <button class="btn">
-            <div class="col-span-full">
-          <label for="cover-photo" class="block text-sm font-medium leading-6 text-gray-900">Cover photo</label>
-          <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-            <div class="text-center">
-              <svg class="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
-                <path fill-rule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clip-rule="evenodd" />
-              </svg>
-              <div class="mt-4 flex text-sm leading-6 text-gray-600">
-                <label for="file-upload" class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
-                  <span>Upload a file</span>
-                  <input id="file-upload" name="file-upload" type="file" class="sr-only"></input>
-                </label>
-                </div>
-                
-            </div>
-            </div>
-            </div>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-upload" viewBox="0 0 16 16">
-  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
-  <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708z"/>
-</svg>
-            </button>
+        <p className="mt-6 text-xl leading-8 text-grey-900 mx-10 mt-10">Please upload your resume down bellow this has to be a PDF file.</p>
+        <div className="col-span-full mt-6">
+              <label htmlFor="cover-photo" className="text-l font-bold tracking-tight sm:text-xl leading-6 text-gray-900 mx-10 ">
+              Resume Upload
+              </label>
+              <div class="mt-2 flex justify-center">
+    <div class="ml-2 rounded-lg border border-solid border-gray-900/50 px-4 py-2 flex items-center" style={{ width: "400px", boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)" }}>
+        <span class="text-xs leading-5 text-gray-600">Upload CV</span>
+    </div>
+    <div class="rounded-lg border border-solid border-gray-900/50 px-1 py-1 flex items-center" style={{  width: "35px", boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)" }}>
+        <label for="file-upload" class="relative cursor-pointer">
+            <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-upload" viewBox="0 0 16 16">
+                <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+                <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708z"/>
+            </svg></span>
+            <input id="file-upload" name="file-upload" type="file" class="sr-only" ></input>
+        </label>
+    </div>
+</div>
+
+
+</div>
+
+           
+
     </>
     
   )
